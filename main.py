5aae412a57e8e9c9c60b1a07f0701bdea693aaf4
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-12-28 11:38:13 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# print("Hello World!")
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# Variables + string interpolation
+
+# character_name = "Meow Meow Beenz"
+# character_age = "21"
+# print("Hi my name is " + character_name)
+# print(character_name + " is " + character_age + " years old")
+
+
+# phrase = "living in a new world with an old soul"
+
+# print(phrase.upper())
+# print(phrase.upper().isupper())
+# print(len(phrase))
+# print(phrase[0])
+
+
+# Numbers
+  # convert number to string
+num = 5
+print(str(num) + " is my fav number bro")
+# absolute value
+nums = -22
+print(abs(nums))
+
